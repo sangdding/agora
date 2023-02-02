433be44ef584c6d69fdc1bc6dd3691a1a2ebd173
sangmyung <enjxz97382@gmail.com>
2023-02-02 18:47:45 +0900

    Feat: Text 기반 채팅 서버

---
+++ src/main/java/capstone/agora/handler/WebSocketHandler.java
@@ -0,0 +1,19 @@
+package capstone.agora.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+        TextMessage textMessage = new TextMessage("Welcome chatting server~");
+        session.sendMessage(textMessage);
+    }
+}
---
+++ src/test/java/capstone/agora/AgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
