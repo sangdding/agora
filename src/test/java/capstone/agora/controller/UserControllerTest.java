51a095af3b8b41ecbd27510824dec8bdc6e1b34c
sangmyung <enjxz97382@gmail.com>
2023-02-21 11:35:39 +0900

    Chore: DB 서버 연결해서 테스트 배포
    작동 완료 확인

--- src/test/java/capstone/agora/controller/UserControllerTest.java
+++ src/test/java/capstone/agora/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package capstone.agora.controller;
 
 import capstone.agora.dto.user.LoginFormDto;
 import capstone.agora.dto.user.RegisterFormDto;
+import capstone.agora.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +26,9 @@ class UserControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
 
     ObjectMapper objectMapper = new ObjectMapper();
 
